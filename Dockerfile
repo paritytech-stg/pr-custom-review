5d5f20eafb8db9700ef5a7cccb3318579fc5679a
Javier Bullrich <javier.bullrich@parity.io>
2023-02-08 15:13:25 +0100

    created test scenario for github action

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18
+
+RUN npm install -g @vercel/ncc
+
+COPY package.json yarn.lock ./
+
+RUN yarn install --frozen-lockfile
+
+COPY . .
+
+RUN ncc build src/action.ts -o dist
+
+CMD ["node", "dist/index.js"]
---
+++ src/action.ts
@@ -0,0 +1,32 @@
+import {debug, error, getInput, info, setFailed} from "@actions/core";
+import {context, getOctokit} from "@actions/github";
+
+const main = async () => {
+    const repoToken = getInput("GITHUB_TOKEN", { required: true });
+
+    const { repo } = context;
+
+    const kit = getOctokit(repoToken);
+    const teams = await kit.rest.repos.listTeams();
+    info(`Data is ${JSON.stringify(teams.data)}`);
+};
+
+
+const errorHandler = (e: Error) => {
+    let er = e;
+    setFailed(e);
+    while (er !== null) {
+        debug(`Stack -> ${er.stack as string}`);
+        if (er.cause != null) {
+            debug("Error has a nested error. Displaying.");
+            er = er.cause as Error;
+            error(er);
+        } else {
+            break;
+        }
+    }
+};
+
+info("Starting action!");
+
+main().then(()=> info("done")).catch(errorHandler)
